6d74718a37c891e06920a81282dc49dee08c36dc
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-11-26 09:51:47 +0600

    Weather Icons

---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Игорь Михайлович Ракитянский on 26/11/2016.
+//  Copyright © 2016 RIM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct CurrentWeather {
+    let temperature: Double
+    let appearentTemperature: Double
+    let humidity: Double
+    let pressure: Double
+    let icon: UIImage
+}
