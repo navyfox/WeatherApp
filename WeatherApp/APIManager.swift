49cbeaedf05e09e9195717e7a928b839fe21564f
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-11-26 16:02:47 +0600

    Protocol extension Default Implementation: Part 1

--- WeatherApp/APIManager.swift
+++ WeatherApp/APIManager.swift
@@ -11,11 +11,54 @@ import Foundation
 typealias JSONTask = URLSessionDataTask
 typealias JSONComletionHandler = ([String: AnyObject]?, HTTPURLResponse?, Error?) -> Void
 
+enum APIResult<T> {
+    case Success(T)
+    case Failure(Error)
+}
+
+
 protocol APIManager {
     var sessionConfiguration: URLSessionConfiguration { get }
     var session: URLSession { get }
 
     func JSONTaskWith(request: URLRequest, completionHandler: JSONComletionHandler) -> JSONTask
+    func fetch<T>(request: URLRequest, parse: ([String: AnyObject]) -> T?, completionHandler: (APIResult<T>) -> Void)
 
     init(sessionConfiguration: URLSessionConfiguration)
+}
+
+extension APIManager {
+    func JSONTaskWith(request: URLRequest, completionHandler: @escaping JSONComletionHandler) -> JSONTask {
+
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+
+                let userInfo = [NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")]
+                let error = NSError(domain: SWINetworkingErrorDomain, code: 100, userInfo: userInfo)
+
+                completionHandler(nil, nil, error)
+                return
+            }
+
+            if data == nil {
+                if let error = error {
+                    completionHandler(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                case 200:
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: AnyObject]
+                        completionHandler(json, HTTPResponse, error)
+                    } catch let error as NSError {
+                        completionHandler(nil, HTTPResponse, error)
+                    }
+                default:
+                    print("We have got response status \(HTTPResponse.statusCode)")
+                }
+            }
+        }
+        return dataTask
+    }
 }
---
+++ WeatherApp/ErrorManager.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorManager.swift
+//  WeatherApp
+//
+//  Created by Игорь Михайлович Ракитянский on 26/11/2016.
+//  Copyright © 2016 RIM. All rights reserved.
+//
+
+import Foundation
+
+public let SWINetworkingErrorDomain = "ru.swiftbook.WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
