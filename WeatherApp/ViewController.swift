0c4930526a136b72faea3200df10528db829a120
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-11-26 09:18:20 +0600

    Available forecast data

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+
+
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +31,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
